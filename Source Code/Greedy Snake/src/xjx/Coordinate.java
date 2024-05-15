d93ea55c649d33c8c193625753911dcec68fd87b
Chen_Scott <95509645+CycScott@users.noreply.github.com>
2024-05-15 11:10:17 +0800

    Source Code

---
+++ Source Code/Greedy Snake/src/xjx/Coordinate.java
@@ -0,0 +1,16 @@
+package xjx;
+
+public class Coordinate {
+    public int x,y;
+
+    public Coordinate(int x0,int y0){
+        x = x0;
+        y = y0;
+    }
+
+    public Coordinate(Coordinate temp){
+        x = temp.x;
+        y = temp.y;
+    }
+}
+
---
+++ Source Code/Greedy Snake/src/xjx/Direction.java
@@ -0,0 +1,5 @@
+package xjx;
+
+public enum Direction {
+    UP, DOWN, LEFT, RIGHT;
+}
